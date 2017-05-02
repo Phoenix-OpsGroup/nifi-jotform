d201925d730e19dee94f2164749aed93592b1574
Tony Winingham <Tony.Winingham@Phoenix-OpsGroup.com>
2017-05-02 07:59:33 -0400

    first commit

---
+++ nifi-jotform-processors/src/main/java/com/phoenixopsgroup/nifi/jotform/JotformReformatJsonProcessor.java
@@ -0,0 +1,137 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phoenixopsgroup.nifi.jotform;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.phoenixopsgroup.jotform.gson.Answer;
+import com.phoenixopsgroup.jotform.gson.AnswerTypeAdapter;
+import com.phoenixopsgroup.jotform.gson.JotformSubmission;
+import com.phoenixopsgroup.jotform.util.JotformResponse;
+import com.phoenixopsgroup.jotform.util.JotformUtil;
+import org.apache.commons.io.IOUtils;
+import org.apache.nifi.annotation.documentation.CapabilityDescription;
+import org.apache.nifi.annotation.documentation.Tags;
+import org.apache.nifi.annotation.lifecycle.OnScheduled;
+import org.apache.nifi.flowfile.FlowFile;
+import org.apache.nifi.logging.ComponentLog;
+import org.apache.nifi.processor.*;
+import org.apache.nifi.processor.exception.ProcessException;
+import org.apache.nifi.processor.io.InputStreamCallback;
+import org.apache.nifi.processor.io.OutputStreamCallback;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicReference;
+
+@Tags({"Jotform", "JSON", "Elasticsearch"})
+@CapabilityDescription("Reformat Jotform JSON suitable for Elasticsearch")
+//@SeeAlso({})
+//@ReadsAttributes({@ReadsAttribute(attribute="", description="")})
+//@WritesAttributes({@WritesAttribute(attribute="", description="")})
+public class JotformReformatJsonProcessor extends AbstractProcessor {
+    
+    public static final Relationship SUCCESS = new Relationship.Builder()
+            .name("SUCCESS")
+            .description("Success relationship")
+            .build();
+
+    public static final Relationship FAIL = new Relationship.Builder()
+            .name("FAIL")
+            .description("Fail relationship")
+            .build();
+    
+    private Set<Relationship> relationships;
+
+    @Override
+    protected void init(final ProcessorInitializationContext context) {
+
+        final Set<Relationship> relationships = new HashSet<Relationship>();
+        relationships.add(SUCCESS);
+        relationships.add(FAIL);
+        
+        this.relationships = Collections.unmodifiableSet(relationships);
+    }
+
+    @Override
+    public Set<Relationship> getRelationships() {
+        return this.relationships;
+    }
+
+    @OnScheduled
+    public void onScheduled(final ProcessContext context) {
+
+    }
+
+    @Override
+    public void onTrigger(final ProcessContext context, final ProcessSession session)
+            throws ProcessException {
+
+        final ComponentLog log = this.getLogger();
+
+        final AtomicReference<String> value = new AtomicReference<>();
+
+        FlowFile flowFile = session.get();
+        if ( flowFile == null ) {
+            return;
+        }
+
+        // We expect to receive a Jotform Submission JSON formatted response
+        // curl -X GET "https://api.jotform.com/submission/{SUBMISSION_ID}?apiKey={API+KEY}"
+        
+        session.read(flowFile, new InputStreamCallback() {
+            @Override
+            public void process(InputStream in) throws IOException
+            {
+                try{
+                    String json = IOUtils.toString(in);
+
+                    final GsonBuilder gsonBuilder = new GsonBuilder();
+                    gsonBuilder.registerTypeAdapter(Answer.class, new AnswerTypeAdapter());
+                    final Gson gson = gsonBuilder.create();
+
+                    JotformSubmission jotformSubmission
+                            = gson.fromJson(json, JotformSubmission.class);
+
+                    JotformUtil jotformUtil = new JotformUtil();
+                    JotformResponse jotformResponse = jotformUtil.getResponse(jotformSubmission);
+                    value.set(jotformResponse.toJson(false));
+
+                }catch(Exception ex){
+                    ex.printStackTrace();
+                    log.error("Failed to read json string.");
+                }
+            }
+        });
+
+        
+        flowFile = session.write(flowFile, new OutputStreamCallback() {
+
+            @Override
+            public void process(OutputStream out) throws IOException {
+                out.write(value.get().getBytes());
+            }
+        });
+
+        session.transfer(flowFile, SUCCESS);
+        
+    }
+}
---
+++ nifi-jotform-processors/src/test/java/com/phoenixopsgroup/nifi/jotform/JotformReformatJsonProcessorTest.java
@@ -0,0 +1,88 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phoenixopsgroup.nifi.jotform;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.nifi.util.MockFlowFile;
+import org.apache.nifi.util.TestRunner;
+import org.apache.nifi.util.TestRunners;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+
+public class JotformReformatJsonProcessorTest
+{
+
+    private TestRunner testRunner;
+
+    @Before
+    public void init() {
+        testRunner = TestRunners.newTestRunner(JotformReformatJsonProcessor.class);
+    }
+
+    @Test
+    public void testProcessor() {
+
+        try
+        {
+            // Mock content
+            URL url = this.getClass().getResource("/data/jotform.json");
+            File file = new File(url.getFile());
+            InputStream content = FileUtils.openInputStream(file);
+
+            // Mock Processor
+            TestRunner runner = TestRunners.newTestRunner(new JotformReformatJsonProcessor());
+
+            // Add Properties
+//            runner.setProperty("key","value");
+
+            // Add content to Mock Processor
+            runner.enqueue(content);
+
+            // Run the content through
+            runner.run(1);
+
+            // All results were processed with out failure
+            runner.assertQueueEmpty();
+
+            // If you need to read or do additional tests on results you can access the content
+            List<MockFlowFile> results = runner.getFlowFilesForRelationship(JotformReformatJsonProcessor.SUCCESS);
+            assertTrue("1 match", results.size() == 1);
+            MockFlowFile result = results.get(0);
+            String resultValue = new String(runner.getContentAsByteArray(result));
+            
+            System.out.println("Match: " + new String(runner.getContentAsByteArray(result)));
+
+            // Test attributes and content
+//            result.assertAttributeEquals(JotformReformatJsonProcessor.MATCH_ATTR, "nifi rocks");
+//            result.assertContentEquals("nifi rocks");
+
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+}
